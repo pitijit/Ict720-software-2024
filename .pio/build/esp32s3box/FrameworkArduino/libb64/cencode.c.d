786c0cff5648668c2a8b4f86836abd1092aa3309
pitijit <85090124+pitijit@users.noreply.github.com>
2024-02-13 11:15:46 +0700

    add changes

---
+++ .pio/build/esp32s3box/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32s3box/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/NECTEC/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/NECTEC/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
